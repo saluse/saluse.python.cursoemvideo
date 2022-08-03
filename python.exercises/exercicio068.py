bae411b6362c3b87cea72c375bf7c2b1a8865178
Weverton <weverton.saluse@gmail.com>
2022-08-02 23:05:10 -0300

    Finalização do exercicio 068.

--- python.exercises/exercicio068.py
+++ python.exercises/exercicio068.py
@@ -1,11 +1,25 @@
 from random import randint
-jogador = int(input('Escolha um número: '))
-pergunta = str(input('Par ou Impar [P/I]: ')).upper().strip()
-computador = randint(1, 10)
-resultado = jogador + computador
-if pergunta == 'Pp' and resultado % 2 == 0:
-    print('O jogador jogou {} e o computador jogou {}: {} é PAR - JOGADOR GANHOU'.format(jogador, computador, resultado))
-elif pergunta == 'Ii' and resultado != 0:
-    print('O computador jogou {} e o jogador jogou {}: {} é PAR - COMPUTADOR GANHOU'.format(computador, jogador, resultado))
-else:
-    print('EMPATE')
+v = 0
+while True:
+    jogador = int(input('Escolha um número: '))
+    computador = randint(0, 10)
+    resultado = jogador + computador
+    tipo = ' '
+    while tipo not in 'PpIi':
+        tipo = str(input('Par ou Impar [P/I]: ')).upper().strip()[0]
+    if tipo == 'P':
+        if resultado % 2 == 0:
+            print('O jogador jogou {} e o computador jogou {}: {} é PAR - JOGADOR GANHOU'.format(jogador, computador, resultado))
+            v += 1
+        else:
+            print('VOCÊ PERDEU, GAME OVER')
+            break
+    elif tipo == 'I':
+        if resultado % 2 == 1:
+            print('O Jogador jogou {} e o computador jogou {}: {} é IMPAR - JOGADOR GANHOU'.format(computador, jogador, resultado))
+            v += 1
+        else:
+            print('VOCÊ PERDEU, GAME OVER')
+            break
+    print('Vamos jogar novamente... ')
+print('Voce venceu {} vezes'.format(v))
