9b225515b1c4c9002d0e25f39971b2799380808f
Weverton <weverton.saluse@gmail.com>
2022-08-03 19:13:24 -0300

    atualização exercicio 069

--- python.exercises/exercicio069.py
+++ python.exercises/exercicio069.py
@@ -1,12 +1,24 @@
 pessoas = 0
+homem = 0
+mulher20 = 0
+continuar = ' '
 while True:
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
-    continuar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
+    if idade <= 18:
+        pessoas += 1
+    if sexo == 'M':
+        homem += 1
+    if sexo == 'F' and idade < 20:
+        mulher20 += 1
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N]: ')).upper().strip()[0]
     if continuar == 'N':
         break
-if idade < 18:
-    print('Ao  total, tem {} pessoas com menos de 18 anos'.format(pessoas))
-    pessoas += 1
-print('FIM')
+print('Ao total foram {} pessoas com menos de 18 anos'.format(pessoas))
+print('Foram cadastrados {} homens'.format(homem))
+print('Ao todo foram {} mulheres com menos de 20 anos'.format(mulher20))
