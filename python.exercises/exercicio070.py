165b8e31add9b5954d32a9eaf054286225ea30a2
Weverton <weverton.saluse@gmail.com>
2022-08-04 20:01:20 -0300

    Atualização exercicio 070

--- python.exercises/exercicio070.py
+++ python.exercises/exercicio070.py
@@ -1,11 +1,29 @@
+preco = 0
+totmil = 0
+soma = 0
+cont = 0
+menor = 0
+print('{:=^40}'.format(' Lojas Barateira '))
 while True:
     nome = str(input('Nome do produto: '))
     preco = float(input('Preço: R$'))
+    cont += 1
+    soma = soma + preco
+    if preco > 1000:
+        totmil = totmil + 1
+    if cont == 1:
+        menor = preco
+    else:
+        if preco < menor:
+            menor = preco
     resp = ' '
     while resp not in 'SN':
         resp = str(input('Quer continuar [S/N]? ')).upper().strip()[0]
         if resp == 'S':
-            print('Digite mais preços: ')
+            print('Digite mais produtos: ')
     if resp == 'N':
         break
-print('FIM')
+print('O totas das suas compras é R$ {:.2f}'.format(soma))
+print('Na lista existem {} produtos que custam mais de R$1.000,00'.format(totmil))
+print('O produto de menor preço custa: R${}'.format(menor))
+print('{:=^40}'.format(' FIM '))
