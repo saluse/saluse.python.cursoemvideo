2665eafe501fbf2f55aa4bb62c76d924cdc2153d
Weverton <weverton.saluse@gmail.com>
2022-08-04 20:42:36 -0300

    exercicio 071

---
+++ python.exercises/exercicio071.py
@@ -0,0 +1,23 @@
+print('=' * 50)
+print('{:^50}'.format('BANCO WS'))
+print('=' * 50)
+valor = int(input('Quanto você quer sacar? R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print('Total de {} cedulas de {} reais'.format(totced, ced))
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
