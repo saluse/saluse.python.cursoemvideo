2e578783cb7db9952131eb2f9c58b45d778642af
Weverton <weverton.saluse@gmail.com>
2022-08-06 22:57:51 -0300

    exercicio072

---
+++ python.exercises/exercicio072.py
@@ -0,0 +1,6 @@
+num = int(input('Digite um número entre 0 e 10: '))
+numero = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez')
+if num < 0 or num > 10:
+    print('Número inválido, digite outro número entre 0 e 10: ')
+else:
+    print(numero[num])
