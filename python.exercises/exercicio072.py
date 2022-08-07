13d93ceb5fbb4250db7c1aa79e7568232277566a
Weverton <weverton.saluse@gmail.com>
2022-08-07 01:29:13 -0300

    atualização072

--- python.exercises/exercicio072.py
+++ python.exercises/exercicio072.py
@@ -23,13 +23,13 @@ sequence = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis',
             'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
-    num = int(input('digite um número:'))
+    num = int(input('digite um número: '))
     if num < 0 or num > 20:
         print('Tente novamente, N: [0/20]')
         continue
     if 0 <= num <= 20:
         print(f'Você digitou o número: {sequence[num]}')
-        continuar = str(input('Você quer continuar?')).strip().lower()[0]
+        continuar = str(input('Você quer continuar? ')).strip().lower()[0]
         if continuar in 'Ss':
             continue
         if continuar in 'Nn':
@@ -37,7 +37,9 @@ while True:
             break
 #More
 
-extensos = ('zero', 'um', 'dois', 'três','quatro', 'cinco', 'seis', 'sete', 'oito','nove','dez', 'onze', 'doze', 'treze', 'quatorze','quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+extensos = ('zero', 'um', 'dois', 'três','quatro', 'cinco', 'seis',
+             'sete', 'oito','nove','dez', 'onze', 'doze', 'treze', 
+             'quatorze','quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 numero = int(input('Digite um numero de 0 a 20: '))
 while numero not in range(0,21):
 	numero = int(input('Tente novamente.Digite um numero de 0 a 20: '))
