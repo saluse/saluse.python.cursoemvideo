83b1ed54b541b23bb67a7e61f4da728801070ab9
Weverton <weverton.saluse@gmail.com>
2022-08-06 22:56:32 -0300

    exercicio072

--- python.exercises/teste001.py
+++ python.exercises/teste001.py
@@ -1,16 +1,8 @@
-'''nome = str(input('Digite seu nome completo: '))
+nome = str(input('Digite seu nome completo: '))
 print('Prazer em te conhecer!')
 ns = nome.split()
 print(ns[0])
-print(ns[len(ns)-1])'''
-
-'''tempo = int(input('Quantos anos tem seu carro? '))
+print(ns[len(ns)-1])
+tempo = int(input('Quantos anos tem seu carro? '))
 if tempo == 
-print('-----FIM-----')'''
-
-
-
-
-
-
-
+print('-----FIM-----')
