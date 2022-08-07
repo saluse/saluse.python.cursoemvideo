d77c6ccfae5c9a2ddcc6ae1927108e1972502327
Weverton <weverton.saluse@gmail.com>
2022-08-07 17:07:07 -0300

    atualização aula 016

--- python.exercises/aula016.py
+++ python.exercises/aula016.py
@@ -17,4 +17,10 @@ c = a + b
 print(c) #retorna o resultado da junção das duas tuplas (a + b)
 print(c.index(5)) #index retorna a posição do número digitado a partir do inicio.
 print(c.index(5, 3)) #retorna a posição do número digitado a partir do 3 elemento.
+print('{:=^50}'.format(' PAUSA '))
+pessoa1 = ('Weverton', 35, 'M', 82)
+pessoa2 = ('Karla', 30, 'F', 62)
+del(pessoa2) #Tuplas são Imutáveis, porem, com o comando del(), podemos apagar a variável entre parenteses
+print(pessoa1) #Tuplas podem ter dados de tipos diferentes dentro das tuplas
+print(pessoa2)
 
