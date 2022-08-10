ec5a3e1ba179da91d35f37f670e71364706d8a5b
Weverton <weverton.saluse@gmail.com>
2022-08-09 22:20:07 -0300

    atualização diesel

--- python.exercises/Diesel.py
+++ python.exercises/Diesel.py
@@ -1,3 +1,4 @@
 diesel = float(input('Valor diesel atual: '))
 kwp = float(input('Valor do Kwh/Ponta: '))
 kwt = float(input('Consumo total em KW: '))
+ktp = float(input('Consumo total na ponta Kwh: '))
