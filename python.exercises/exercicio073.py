bc5df4c9c9c9cb2860f6116b970f20e9052ab8eb
Weverton <weverton.saluse@gmail.com>
2022-08-11 22:16:46 -0300

    Atualização Exercicio 073

--- python.exercises/exercicio073.py
+++ python.exercises/exercicio073.py
@@ -2,14 +2,14 @@ cont = 6
 tab = ('Palmeiras', 'Corinthians', 'Fluminense', 'Flamengo', 'Atlético-PR', 'Internacional', 'Atlético Mineiro', 'Bragantino', 'América-MG', 'Santos')
 for pos, time in enumerate(tab[:5]):
     print('O {} é o {}º colocado da tabela do Brasileirão'.format(time, pos + 1))
-print('=' * 75)
+print('=' * 150)
 for pos, time in enumerate(tab[5:]):
     print('O {} é o {}º da tabela do Brasileirão'.format(time, cont))
     cont += 1
-print('=' * 75)
+print('=' * 150)
 print('========== SEGUE ABAIXO OS TIMES DA TABELA EM ORDEM ALFABÉTICA ============')
-print('=' * 75)
+print('=' * 150)
 print(sorted(tab))
 print('=' * 75)
 print('O Internacional está na {}º posição'.format(tab.index('Internacional')+1))
-print('=' * 75)
+print('=' * 150)
