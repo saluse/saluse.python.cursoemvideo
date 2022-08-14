5af6586b0586d74cfb47b0d53d6d3f0b26790c6f
Weverton <weverton.saluse@gmail.com>
2022-08-14 20:32:05 -0300

    Exercicio Diesel Atualização

--- python.exercises/Diesel.py
+++ python.exercises/Diesel.py
@@ -2,8 +2,9 @@ diesel = float(input('Valor diesel atual: '))
 kwp = float(input('Valor do Kwh/Ponta: '))
 kwt = float(input('Consumo total em KW: '))
 ktp = float(input('Consumo total na ponta Kwh: '))
+dd = 100 * 6.5
 Consumopontad = diesel * ktp
 consumopontae = ktp * kwp
-print('O valor do consumo em reais com o gerador operando na ponta é de R${}'.format(Consumopontad))
+print('O valor do consumo em reais com o gerador operando na ponta é de R${}'.format(dd))
 print('O valor do consumo em reais sem o gerador operando na ponta é de R${}'.format(consumopontae))
 print('FIM')
