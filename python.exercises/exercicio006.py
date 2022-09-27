fb9564d3f9e09b0ccb832439132879e1747cbcf7
Weverton Seixas <weverton.saluse@gmail.com>
2022-09-27 18:07:33 -0300

    Atualização de Portifólio - Multiplicações

--- python.exercises/exercicio006.py
+++ python.exercises/exercicio006.py
@@ -1,16 +1,14 @@
+from time import sleep
+print('Oi!')
+sleep(1)
+print('Vamos exercitar a matemática?')
+sleep(1)
 n = int(input('Digite um número: '))
+print('Qual o dobro, o triplo e a raiz quadrada do número informado')
+sleep(3)
+print('Não se preocupe, vou te ajudar')
+sleep(2)
 d = n * 2
 t = n * 3
 r = n ** (1/2)
 print('O dobro de {} é {}, o  triplo de {} é {}, a raiz quadrada é {:.2f}'.format(n, d, n, t, r))
-
-
-
-
-
-
-
-
-
-
-
