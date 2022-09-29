f87b2e84d8e5ecf316fa8852c1031877af20dfd4
Weverton Seixas <weverton.saluse@gmail.com>
2022-09-28 23:33:16 -0300

    Atualização de Portifólio - Conversor de Grandezas

--- python.exercises/exercicio008.py
+++ python.exercises/exercicio008.py
@@ -1,7 +1,11 @@
+from time import sleep
+print('Vamos Converter alguns valores distanciais')
 d = float(input('Uma distância em metros: '))
 k = d/1000
 print('A medida de {}m corresponde a {}Km: '.format(d, k))
+sleep(1)
 print('A medida de {}m corresponde a {}Dm: '.format(d, d/100))
+sleep(1)
 print('A medida de {}m corresponde a {}cm: '.format(d, d*1000))
 
 
