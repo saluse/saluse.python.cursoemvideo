498e73ba2a1db6d5004aaa426d480bbf4a2a8a13
Weverton Seixas <weverton.saluse@gmail.com>
2022-10-01 21:51:57 -0300

    Atualização de Portifólio - Reajuste Salarial

--- python.exercises/exercicio013.py
+++ python.exercises/exercicio013.py
@@ -1,3 +1,4 @@
+print('REAJUSTE DE 15%')
 s = float(input('Qual é o salário do funcionário? R$'))
 a = s + (s * 15 / 100)
 print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(s, a))
